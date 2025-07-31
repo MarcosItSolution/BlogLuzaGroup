d2b6557fc0485e6640d565f7d2623db37dca7295
Marcos Silva <marcos.silva.itsolution@segurosunimed.com.br>
2025-07-30 21:55:58 -0300

    validando de existe, antes de criar as shadowProperties, para evitar erro de criação duplicado

--- Blog.Infrastructure/Abstractions/ShadowProperties.cs
+++ Blog.Infrastructure/Abstractions/ShadowProperties.cs
@@ -4,18 +4,23 @@ namespace Blog.External.Infrastructures.Persistences.Abstractions
 {
     public static class ShadowProperties
     {
-
         public static void Apply(ModelBuilder modelBuilder)
         {
             var allEntities = modelBuilder.Model.GetEntityTypes();
 
             foreach (var entity in allEntities)
             {
-                entity.AddProperty("CreatedDate", typeof(DateTime));
-                entity.AddProperty("UpdatedDate", typeof(DateTime));
-                entity.AddProperty("DeletedDate", typeof(DateTime?));
+                if (entity.FindProperty("CreatedDate") == null)
+                    entity.AddProperty("CreatedDate", typeof(DateTime));
+
+                if (entity.FindProperty("UpdatedDate") == null)
+                    entity.AddProperty("UpdatedDate", typeof(DateTime));
+
+                if (entity.FindProperty("DeletedDate") == null)
+                    entity.AddProperty("DeletedDate", typeof(DateTime?));
             }
         }
+
     }
 }
 
